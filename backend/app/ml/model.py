8a3b3fa63a899291fbad5221201586e1b07315ca
maslesa <ljubomirmaslesa@gmail.com>
2026-08-11 23:07:04 +0200

    [chore]: prepare backend for deployment

--- backend/app/ml/model.py
+++ backend/app/ml/model.py
@@ -1,18 +1,20 @@
 import torch
 from torchvision.models import convnext_tiny
-from pathlib import Path
+from huggingface_hub import hf_hub_download
 
 NUM_CLASSES = 46
 
-MODEL_PATH = (
-    Path(__file__).resolve().parents[3]
-    / 'models'
-    / 'best_convnext_tiny_hyperparameter_tuned.pth'
-)
+HF_REPO_ID = 'maslesa/mushroom-convnext-tiny'
+MODEL_FILENAME = 'best_convnext_tiny_hyperparameter_tuned.pth'
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def load_model():
+    model_path = hf_hub_download(
+        repo_id=HF_REPO_ID,
+        filename=MODEL_FILENAME
+    )
+
     model = convnext_tiny(
         weights=None
     )
@@ -23,12 +25,17 @@ def load_model():
     )
 
     checkpoint = torch.load(
-        MODEL_PATH,
+        model_path,
         map_location=DEVICE
     )
 
-    if isinstance(checkpoint, dict) and 'model_state_dict' in checkpoint:
-        model.load_state_dict(checkpoint['model_state_dict'])
+    if (
+        isinstance(checkpoint, dict)
+        and 'model_state_dict' in checkpoint
+    ):
+        model.load_state_dict(
+            checkpoint['model_state_dict']
+        )
     else:
         model.load_state_dict(checkpoint)
 
