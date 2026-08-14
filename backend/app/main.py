613252717f6b59e73c73583d7f977abf456c6043
maslesa <ljubomirmaslesa@gmail.com>
2026-08-14 21:39:09 +0200

    [fix]: fix CORS middleware and add frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,33 +15,29 @@ app = FastAPI(
     version='1.0.0'
 )
 
+frontend_url = os.getenv('FRONTEND_URL')
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        os.getenv(
-            'FRONTEND_URL',
-            ''
-        )
+        frontend_url
+    ] if frontend_url else [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173'
     ],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*']
 )
 
-
-app.include_router(
-    router,
-    prefix='/api'
-)
+app.include_router(router, prefix='/api')
 
 
 @app.get('/health')
 def health_check():
     return {
         'status': 'ok',
-        'model': 'ConvNeXt Tiny',
-        'device': 'cpu'
+        'model': 'ConvNeXt Tiny'
     }
